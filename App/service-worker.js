6e71aef112a7e752aa1e8996993321ef8badffee
S. Kabajah <shadi.kabajah@gmail.com>
2025-10-20 16:42:06 -0400

    remove servicer

--- App/service-worker.js
+++
@@ -1,8 +0,0 @@
-// Minimal Service Worker
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
-});
-
-self.addEventListener('fetch', (event) => {
-  // Optional: caching logic can go here
-});
